e892c14e2ba2fe7e8fa418c8a21f0687235d31af
teinbusrabudan <busra.budan@tein.com.tr>
2022-10-31 16:39:06 +0300

    config added

---
+++ library-service/src/main/java/com/kitaplik/libraryservice/model/Library.kt
@@ -0,0 +1,17 @@
+package com.kitaplik.libraryservice.model
+
+import org.hibernate.annotations.GenericGenerator
+import javax.persistence.Column
+import javax.persistence.ElementCollection
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+
+data class Library @JvmOverloads constructor(
+        @Id
+        @Column(name = "library_id")
+        @GeneratedValue(generator = "UUID")
+        @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+        val id: String? = "",
+        @ElementCollection
+        val userBook: List<String> = ArrayList()
+)
